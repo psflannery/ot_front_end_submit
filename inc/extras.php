b64a421daaa5ce7d0b89a3254eb48e57a9293e28
psflannery <psflannery@gmail.com>
2015-10-11 18:54:39 +0100

    "submitted by" label

--- inc/extras.php
+++ inc/extras.php
@@ -31,9 +31,10 @@ add_filter( 'post_class', 'opening_times_bv_link_submit_post_classes', 10, 3 );
 function opening_times_bv_link_submitter() {
 	global $post;
 	$submitted_by = get_post_meta( $post->ID, '_ot_bv_link_submit_name', true );
+	$label = esc_html__( 'Submitted by: ', 'opening_times' );
 	if ( '' != $submitted_by ) :
-		echo $submitted_by;
+		echo $label, $submitted_by;
 	else :
-		esc_html_e( 'Anonymous', 'opening_times' );
+		echo $label, esc_html__( 'Anonymous', 'opening_times' );
 	endif;
 }
--- ot_front_end_submit.php
+++ ot_front_end_submit.php
@@ -12,23 +12,13 @@
  *
  * @wordpress-plugin
  *
-<<<<<<< Updated upstream
- * Plugin Name:	      OT front end submission form
- * Plugin URI:	      http://otdac.org
- * Description:	      Adds a front end submission form to the site via a short-code, so that visitors can add their own content.
- * Version: 	      1.2.1
- * Author: 		      Paul Flannery
- * Author URI:	      http://paulflannery.co.uk
- * Text Domain:	      opening_times
-=======
  * Plugin Name:       OT front end submission form
  * Plugin URI:        http://otdac.org
  * Description:       Adds a front end submission form to the site via a short-code, so that visitors can add their own content.
- * Version:           1.2.2
+ * Version:           1.2.3
  * Author:            Paul Flannery
  * Author URI:        http://paulflannery.co.uk
  * Text Domain:       opening_times
->>>>>>> Stashed changes
  * License:           GPL-2.0+
  * GitHub Plugin URI: https://github.com/psflannery/ot_front_end_submit
  * GitHub Branch:     master
