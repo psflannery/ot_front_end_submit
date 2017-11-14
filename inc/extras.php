e3f60783b2f9ada3268595fb2bc17e5486c117df
psflannery <psflannery@gmail.com>
2017-11-14 21:30:44 +0000

    update button styles

--- inc/extras.php
+++ inc/extras.php
@@ -5,36 +5,22 @@
  * @package: OT front end submission form
  * @subpackage: Opening Times
  */
-
  
 /**
- * Adds custom classes to the array of post classes.
- *
- * @param array $classes Classes for the posts.
- * @return array
- *
+ * Return the the name of the person who submitted a link
  */
-function opening_times_bv_link_submit_post_classes( $classes, $class, $post_id ) {
-    if ( is_page_template( 'bv-submitted-links.php' )  ) {
-		$classes[] = 'strap-container';
-		$classes[] = 'veiled';
-    }
- 
-    return $classes;
-}
-add_filter( 'post_class', 'opening_times_bv_link_submit_post_classes', 10, 3 );
-
- 
-/**
- * Output the the name of the person who submitted a link
- */
-function opening_times_bv_link_submitter() {
-	global $post;
-	$submitted_by = get_post_meta( $post->ID, '_ot_bv_link_submit_name', true );
+function opening_times_get_link_submitter() {
+	$submitted_by = get_post_meta( get_the_ID(), '_ot_bv_link_submit_name', true );
 	$label = esc_html__( 'Submitted by: ', 'opening_times' );
-	if ( '' != $submitted_by ) :
-		echo $label, $submitted_by;
-	else :
-		echo $label, esc_html__( 'Anonymous', 'opening_times' );
-	endif;
+
+	$name = '' != $submitted_by ? $submitted_by : esc_html__( 'Anonymous', 'opening_times' );
+
+	return $label . $name ;
+}
+
+/**
+ * Echo the the name of the person who submitted a link
+ */
+function opening_times_do_link_submitter() {
+	echo opening_times_get_link_submitter();
 }
